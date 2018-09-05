62b88e903576fff0bda2ae350486384129dab84b
lsolagc@hotmail.com <lsolagc@hotmail.com>
2018-09-05 17:01:38 -0300

    #3 - Done
    Adicionada a função MainActivity.createDatabase().

--- AppTG/wear/src/main/java/com/tg/lucas/apptg/MainActivity.java
+++ AppTG/wear/src/main/java/com/tg/lucas/apptg/MainActivity.java
@@ -1,5 +1,7 @@
 package com.tg.lucas.apptg;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.util.Log;
@@ -21,6 +23,20 @@ public class MainActivity extends WearableActivity {
     }
 
     private void createDatabase() {
+        Log.d(TAG, "createDatabase: init");
+        CoordinatesDbHelper dbHelper = new CoordinatesDbHelper(getApplicationContext());
 
+        // Gets the data repository in write mode
+        dbHelper.getWritableDatabase();
+
+        // Create a new map of values, where column names are the keys
+        /*
+        ContentValues values = new ContentValues();
+        values.put(CoordinatesContract.CoordinatesEntry.COLUMN_NODE_ID, 1);
+
+
+        // Insert the new row, returning the primary key value of the new row
+        long newRowId = db.insert(CoordinatesContract.CoordinatesEntry.TABLE_NAME, null, values);
+        */
     }
 }
